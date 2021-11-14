272c351ae7e0d1cb8f707b5c89b6a443d1b824d6
unknown <simplezhang@yeahka.com>
2021-11-14 19:40:17 +0800

    fix: swjs

--- src/index.tsx
+++ src/index.tsx
@@ -24,15 +24,6 @@ const theme = createTheme({
     },
   }
 }); 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('@/sw.js').then(registration => {
-      console.log('SW registered: ', registration);
-    }).catch(registrationError => {
-      console.log('SW registration failed: ', registrationError);
-    });
-  });
-}
 
 ReactDOM.render(
       <Provider store={store}>
